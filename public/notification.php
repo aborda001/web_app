47aae5e41696aa96acc0251cdd6bfe3e14868643
Arnaldo Borda <aborda001@gmail.com>
2022-05-18 04:11:01 -0400

    Creating notification system

---
+++ public/notification.php
@@ -0,0 +1,16 @@
+<?php
+function showNotify()
+{
+  if (isset($_COOKIE['message'])) {
+    echo "<div class='alert " . $_COOKIE['alert'] . " alert-dismissible fade show' role='alert'>";
+    echo $_COOKIE['message'];
+    echo "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>";
+    echo "</div>";
+  }
+}
+
+function setNotify($message, $alert)
+{
+  setcookie("message", $message, time() + 5);
+  setcookie("alert", $alert, time() + 5);
+}
