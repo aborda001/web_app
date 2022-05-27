4f63f979ace542d88d8430da51214f7a77004944
Arnaldo Borda <aborda001@gmail.com>
2022-05-27 16:02:46 -0400

    Fixing sidebar error in mobile mode

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,15 @@ sidebarToggle.addEventListener("click", function () {
   document.querySelector("body").classList.toggle("active");
   document.getElementById("sidebarToggle").classList.toggle("active");
 });
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth < 768) {
+    document.querySelector("body").classList.toggle("active");
+    document.getElementById("sidebarToggle").classList.toggle("active");
+  }
+});
+
+if (window.innerWidth < 768) {
+  document.querySelector("body").classList.toggle("active");
+  document.getElementById("sidebarToggle").classList.toggle("active");
+}
