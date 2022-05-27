14af0d5718c130de59b26d4f93fe98f194f1a841
Arnaldo Borda <aborda001@gmail.com>
2022-05-27 01:06:46 -0400

    Creating subjects list view

---
+++ public/subjects-list.php
@@ -0,0 +1,83 @@
+<?php
+$actived = [' ', '', '', ' active'];
+$title = 'Listado materias';
+require_once("../resources/config.php");
+require_once(TEMPLATES_PATH . "/panel.php");
+require_once(TEMPLATES_PATH . "/navbar.php");
+include LIBRARY_PATH . '/db.php';
+?>
+<div class="container position-relative pt-5">
+  <div class="row">
+    <div class="col-md-5 mt-3">
+      <?php
+      include NOTIFY_PATH;
+      shownotify();
+
+      $connection = connect($config);
+      $query = "SELECT subjects.id, subjects.name AS subject_name, subjects.hours, teachers.name, teachers.lastname from subjects JOIN teachers ON subjects.teacher_id = teachers.id;";
+
+      $response = mysqli_query($connection, $query);
+      mysqli_close($connection);
+
+      ?>
+    </div>
+
+    <div class="position-relative">
+      <h1 class="text-center  mt-5">Listado de materias</h1>
+      <br>
+      <div class="table-responsive">
+
+        <table class="table table-striped align-middle">
+          <thead>
+            <tr>
+              <th scope="col">N</th>
+              <th scope="col">Materia</th>
+              <th scope="col">Horas cátedras</th>
+              <th scope="col">Docente</th>
+              <th scope="col">Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $index = 1;
+            while ($row = mysqli_fetch_assoc($response)) {
+              $teacher = $row['name'] . " " . $row['lastname'];
+              $hours = $row['hours'] . ($row['hours'] > 1 ? " horas" : " hora");
+              echo "
+                <tr>
+                  <th scope='row'>" . $index++ . "</th>
+                  <td>" . $row['subject_name'] . "</td>
+                  <td>" . $hours . "</td>
+                  <td>" . $teacher . "</td>
+                  <td>
+                    <a href='". BASE_URL."/update-subjects.php?id=" . $row['id'] . "' class='btn btn-success mx-1 my-1'> <i class='fas fa-pen'></i> </a>
+                    <a href='". BASE_URL."/delete.php?id=" . $row['id'] . "&table=subjects' class='btn btn-danger mx-1 my-1'> <i class='fas fa-trash'></i> </a>
+                  </td>
+                </tr>";
+            }
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+  document.addEventListener('DOMContentLoaded', () => {
+    const btnDelete = document.querySelectorAll('.btn-danger');
+    if (btnDelete) {
+      const btnArray = Array.from(btnDelete);
+      btnArray.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+          if (!confirm('Estás seguro de eliminarlo?')) {
+            e.preventDefault();
+          }
+        });
+      })
+    }
+  });
+</script>
+
+<?php
+require_once(TEMPLATES_PATH . "/footer.php");
+?>
